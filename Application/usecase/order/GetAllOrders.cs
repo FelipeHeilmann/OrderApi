c73f02a6b628b1df05af859cbf70d2e1faeffbdc
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-11-18 01:49:32 -0300

    feat: get all orders

---
+++ Application/usecase/order/GetAllOrders.cs
@@ -0,0 +1,23 @@
+﻿using Application.repository;
+using Domain.order.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.usecase.order
+{
+    public class GetAllOrders
+    {
+        private readonly OrderRepository _repository;
+        public GetAllOrders(OrderRepository repository) => _repository = repository;
+
+        public async Task<List<Order>> Execute()
+        {
+            var products = await _repository.GetAll();
+
+            return products;
+        }
+    }
+}
